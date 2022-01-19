a083163a5892b99ed18e3f7fdceda7d282b58df8
Rodrigo <rodrigosantos850@hotmail.com>
2022-01-19 09:11:01 -0300

    Projeto iniciado

---
+++ Primeiro/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System;
+using System.Collections.Generic;
+
+namespace Primeiro
+{
+    class primeiro
+    {
+
+       static void Main (string[] args)
+        {
+
+            Console.WriteLine("Digite dois números M e N: ");
+            string[] line = Console.ReadLine().Split (' ');
+            int m = int.Parse (line[0]);
+            int n = int.Parse (line[1]);
+            int [,] mat = new int [m, n];
+            for(int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine ().Split (' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat [i,j] = int.Parse (values[j]);
+                }
+            }
+
+            Console.WriteLine("Digite um número inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < m; ++i)
+            {
+                for(int j=0; j < n; j++)
+                {
+                    if(mat [i,j] == x)
+                    {
+                        Console.WriteLine("Posição: "+"("+ i + ", " + j+")");
+                    }
+                    if(j > 0)
+                    {
+                        Console.WriteLine("Left: " + mat[i, j - 1]);
+                    }
+                    if(i > 0)
+                    {
+                        Console.WriteLine("Up: " + mat[i - 1, j]);
+                    }
+                    if (j < n)
+                    {
+                        Console.WriteLine("Right: " + mat[i, j + 1]);
+                    }
+                    if (i < m - 1)
+                    {
+                        Console.WriteLine("Down: " + mat[i + 1, j]);
+                    }
+                    
+                }
+            }
+            
+
+        }
+
+    }
+            
+       
+}
+
+
+
+    
+
---
+++ Primeiro/Triangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Employee
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+        
+        public void Salario (double porcentagem)
+        {
+            Salary += Salary * porcentagem / 100.0;
+        }
+
+
+        public override string ToString()
+        {
+            return ID
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+             
+    }
+
+        
+        
+    }
+
